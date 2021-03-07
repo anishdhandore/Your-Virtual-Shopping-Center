d99db22a538d3c34b76af3cf0032f6a662b6226b
Anish <anish.dhandore@gmail.com>
2021-03-08 00:51:30 +0530

    models

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    model_id = models.AutoField(primary_key=True)
+    model_name = models.CharField(max_length=100)
+    desc = models.TextField(max_length=200)
+    pub_date = models.DateField()
+

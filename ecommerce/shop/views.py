d8825e9e0e7fc9333dfd561852eac7422f48683e
Anish <anish.dhandore@gmail.com>
2021-03-11 04:03:16 +0530

    logic for displaying all products on home page

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, "shop/index.html")
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    slides = n//4 + 1
+
+    # parameters = {'no_of_slides': slides, 'range': range(1, slides), 'product': products}
+    allProducts = [[products, range(1, slides), slides], [products, range(1, slides), slides]]
+    parameters = {'allProducts': allProducts}
+    return render(request, "shop/index.html", context=parameters)
 
 def about(request):
     return render(request, "shop/about.html")
